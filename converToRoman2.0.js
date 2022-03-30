64aa99bc85f583de34ef96b2fb023693bc85eb48
Fauzan Mursalin <zuchairul@gmail.com>
2022-03-30 20:27:26 +0800

    first commit

---
+++ converToRoman2.0.js
@@ -0,0 +1,30 @@
+function converToRoman(num) {
+    let getNum = num
+
+    const roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    }
+
+    let str = ''
+
+    for(let i of Object.keys(roman)) {
+        let numToRepeat = Math.floor(getNum / roman[i])
+        getNum -= numToRepeat * roman[i]
+        str += i.repeat(numToRepeat)
+    }
+    return str
+}
+
+console.log(converToRoman(2374));
